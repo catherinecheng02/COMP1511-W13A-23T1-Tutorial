7097bc8d2fc6b48edf29e6ad923c3c9562c7aede
catherinecheng02 <z5310517@ad.unsw.edu.au>
2023-03-01 14:14:57 +1100

    Create loop.c

---
+++ tute03/loop.c
@@ -0,0 +1,14 @@
+// While loop demo
+// Written by Catherine Cheng 01-03-2023
+#include <stdio.h>
+int main(void) {
+    // initialise a counter
+    int i = 0;
+    // initial condition
+    while (i < 5) {
+        printf("%d\n", i);// do something
+        i = i + 1; //increment (can also do i++)
+    }
+
+    return 0;
+}
