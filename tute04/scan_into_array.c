bedb7886468b3817a2a9056ca5ae9e76763a07c7
catherinecheng02 <z5310517@ad.unsw.edu.au>
2023-03-08 14:21:53 +1100

    Create scan_into_array.c

---
+++ tute04/scan_into_array.c
@@ -0,0 +1,15 @@
+// Scanning fixed number of inputs into array
+// Written by Catherine Cheng (z5310517)
+
+#include <stdio.h>
+#define SIZE 10
+int main(void) {
+    // We are going to scan 10 numbers in and find the maximum number
+    int array[SIZE];
+    int i = 0;
+    while (i < SIZE) {
+        scanf("%d", &array[i])
+        i++;
+    }
+    
+}
