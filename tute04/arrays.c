1e24649236db5a0ffd59844ac25d19f94181b029
catherinecheng02 <z5310517@ad.unsw.edu.au>
2023-03-08 14:21:25 +1100

    Create arrays.c

---
+++ tute04/arrays.c
@@ -0,0 +1,23 @@
+// Arrays demo 
+// Written by Catherine Cheng (z5310517)
+
+#include <stdio.h>
+#define SIZE 3
+int main(void) {
+    // What is an array?
+    // data type
+    // collection of variables
+
+    // How do we initialise an array?
+    int my_array[SIZE] = {7, 3, 1};
+
+    // How do we print out an array?
+    int i = 0;
+    while (i < SIZE) {
+        printf("%d\n", array[i]);
+        i++;
+    }
+    
+    
+    return 0;
+}
